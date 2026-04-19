0725ab86b7e144977a59edcef53d87ca91f912d4
rajakaman418 <rajakaman418@gmail.com>
2026-04-19 11:03:53 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://infotact-solution-intern.onrender.com",
 });
 
 client.interceptors.request.use((config) => {
