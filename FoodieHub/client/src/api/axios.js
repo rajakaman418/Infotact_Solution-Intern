9c5b1a371de31b59794cb64989a9cc7b76403e4f
Dibakar Rajak <dibakarrajak.cse.23@sittechno.edu.in>
2026-05-12 11:45:02 +0530

    adding url

--- FoodieHub/client/src/api/axios.js
+++ FoodieHub/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://foodiehub-6b9b.onrender.com/api',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
